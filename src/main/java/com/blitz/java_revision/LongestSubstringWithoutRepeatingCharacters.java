9b6e502a58aedf36de4c11936f459edfb231a56e
dev-blitz <abhinab.duttagupta@gmail.com>
2026-02-07 18:26:17 +0530

    code for LongestSubstringWithoutRepeatingCharacters

---
+++ src/main/java/com/blitz/java_revision/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+package com.blitz.java_revision;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+  public static int longestSubstringWithoutRepeatingCharacters(String s) {
+    Set<Character> set = new HashSet<Character>();
+    int max = 0;
+    int left = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+      char c = s.charAt(right);
+      while (set.contains(c))
+          set.remove(s.charAt(left++));
+      set.add(c);
+      max = max > (right - left + 1) ? max : (right - left + 1);
+    }
+    return max;
+  }
+}
