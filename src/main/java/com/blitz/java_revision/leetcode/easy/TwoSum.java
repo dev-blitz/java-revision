45f5ee20be0188793dc04a9265057b3ddfdde37c
dev-blitz <abhinab.duttagupta@gmail.com>
2026-03-08 10:39:52 +0530

    corrected documentation for TwoSum.java

--- src/main/java/com/blitz/java_revision/leetcode/easy/TwoSum.java
+++ src/main/java/com/blitz/java_revision/leetcode/easy/TwoSum.java
@@ -3,7 +3,22 @@ package com.blitz.java_revision.leetcode.easy;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Solution for the Two Sum problem.
+ * * <p>This implementation uses a {@link java.util.HashMap} to achieve 
+ * linear time complexity {@code O(n)}.</p>
+ * * @see <a href="https://leetcode.com/problems/two-sum/">LeetCode Problem Link</a>
+ * @author blitz
+ */
 public class TwoSum {
+  
+  /**
+   * Finds two indices such that the values at those indices sum to the target.
+   * * @param array  The input array of integers.
+   * @param target The integer value to reach.
+   * @return An array containing the two indices, or {@code [-1, -1]} if 
+   * no solution exists.
+   */
   public static int[] twoSum(int[] array, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < array.length; i++) {
